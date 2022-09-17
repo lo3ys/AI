71b18c1193c443d54e67d899353de3ede338f5a6
netscape-swega <34111613+netscape-swega@users.noreply.github.com>
2022-09-17 19:48:27 +0200

    [a] Unit test for utilities

---
+++ JAVA/tests/MatrixTest.java
@@ -0,0 +1,169 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MatrixTest {
+
+	@Test
+	void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMatrixIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMatrixInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMatrixFloatArrayArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMatrixMatrix() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMatrixFloatArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqual() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAssignFloatArrayArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAssignMatrix() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTranspose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSubstract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFactor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFusion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRandomize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRandomizeFloatFloat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMutateFloatFloatFloat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMutateFloat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCrossover() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNormalize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExtend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClampIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClampInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMap() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ JAVA/tests/functionsTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class functionsTest {
+
+	@Test
+	void testFunctions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testApply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDerivative() {
+		fail("Not yet implemented");
+	}
+
+}
